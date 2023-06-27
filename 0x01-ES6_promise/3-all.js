fdfbd7b6768da72800aa1a47e2180b085d1b7fd9
root <adjerusa@gmail.com>
2023-06-27 10:56:36 -0700

    task

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,19 +1,15 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-	let firstName;
-	let lastName;
-	let body;
-
-	const p = uploadPhoto();
-	const u = createUser();
-
-	return Promise.all([p, u]).then((data) => {
-		body = data[0].body;
-		firstName = data[1].firstName;
-		lastName = data[1].lastName;
-		console.log(`${body} ${firstName} ${lastName}`);
-		}).catch(() => {
-			console.log('Signup system offline');
-		});
+	
+	const photo = uploadPhoto();
+	const user = createUser();
+	
+	return Promise.all([photo, user])
+	.then((data) => {
+		console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+	})
+	.catch(() => {
+		console.log('Signup system offline');
+	});
 }
